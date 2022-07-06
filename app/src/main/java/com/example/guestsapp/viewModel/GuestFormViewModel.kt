e605990a07e0c68844438b706ed03abef488bdd3
FelipeEnne <felipeenne@gmail.com>
2022-07-06 19:45:06 -0300

    call insert

--- app/src/main/java/com/example/guestsapp/viewModel/GuestFormViewModel.kt
+++ app/src/main/java/com/example/guestsapp/viewModel/GuestFormViewModel.kt
@@ -3,9 +3,14 @@ package com.example.guestsapp.viewModel
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.ViewModel
+import com.example.guestsapp.Model.GuestModel
 import com.example.guestsapp.repository.GuestRepository
 
 class GuestFormViewModel(application: Application) : AndroidViewModel(application) {
     private val repository = GuestRepository.getInstance(application)
+
+    fun insert(guest: GuestModel) {
+        repository.insert(guest)
+    }
 
 }
