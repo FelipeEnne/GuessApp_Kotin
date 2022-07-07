52a07f392489c704299c1723c6690a629af64a3c
FelipeEnne <felipeenne@gmail.com>
2022-07-07 20:31:22 -0300

    implement update and delete

--- app/src/main/java/com/example/guestsapp/repository/GuestRepository.kt
+++ app/src/main/java/com/example/guestsapp/repository/GuestRepository.kt
@@ -4,6 +4,7 @@ import android.content.ContentValues
 import android.content.Context
 import com.example.guestsapp.Contants.DataBaseConstants
 import com.example.guestsapp.Model.GuestModel
+import kotlinx.coroutines.selects.select
 import java.lang.Exception
 
 class GuestRepository private constructor(context: Context) {
@@ -36,7 +37,40 @@ class GuestRepository private constructor(context: Context) {
         } catch (e: Exception) {
             false
         }
+    }
 
+    fun update(guest: GuestModel) {
+        try {
+            val db = guestDataBase.writableDatabase
+            val values = ContentValues()
+
+            val presence = if (guest.presence) 1 else 0
+            values.put( DataBaseConstants.GUEST.COLUMNS.NAME, guest.name)
+            values.put( DataBaseConstants.GUEST.COLUMNS.PRESENCE, presence)
+
+            val selection = DataBaseConstants.GUEST.COLUMNS.ID + " = ?"
+            val args = arrayOf(guest.id.toString())
+
+            db.update(DataBaseConstants.GUEST.TABLE_NAME, values, selection, args)
+            true
+        } catch (e: Exception) {
+            false
+        }
+    }
+
+    fun delete(id: Int) {
+        try {
+            val db = guestDataBase.writableDatabase
+            val values = ContentValues()
+
+            val selection = DataBaseConstants.GUEST.COLUMNS.ID + " = ?"
+            val args = arrayOf(id.toString())
+
+            db.delete(DataBaseConstants.GUEST.TABLE_NAME, selection, args)
+            true
+        } catch (e: Exception) {
+            false
+        }
     }
 
 }
